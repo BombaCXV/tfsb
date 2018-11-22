66598cb90e5b491b1e12513abdc5145c4315d37a
BombaCXV <denis.orekhov@yahoo.de>
2018-11-22 01:21:44 +0100

    Add files via upload

---
+++ keys.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 22 01:05:30 2018
+
+@author: Denis
+"""
+
+CONSUMER_KEY = 'viojt1UgnJ73d6Tk44S2Y1Qqy'
+CONSUMER_SECRET = 'hxXSOvuU5Wkzntcthm6dV7YrnSf6lrOtniAmeePHR6E8OFzyA0'
+ACCESS_KEY = '1064672166795448320-YwGkEvTqVZV2Jsyrra8BbJzalbOLGV'
+ACCESS_SECRET = 'jA8FgRjP9YM6oyxkQkIHSNc8G8SIZbvXGPaZhG4DvMwdr'
